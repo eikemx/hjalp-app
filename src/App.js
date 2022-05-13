479f66102259bdda8be56b47d365a45651244cb3
Eike mx <merxeike@gmail.com>
2022-05-13 08:16:03 +0100

    final mockdata

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './index.css';
 import hospitals from "./hospitals.json";
 
+import Card from './components/Cards';
+
 
 const App = () => {
   console.log(hospitals);
   return (
     <div className="App">
       <header>hjálp</header>
+      <Card/>
     </div>
   );
 }
